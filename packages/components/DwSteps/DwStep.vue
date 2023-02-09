46b6e9fefc58a3d297ac5ec3ad649f2f4f46f619
蔡博 <306048913@qq.com>
2023-02-09 17:04:55 +0800

    修改step组件bug

--- packages/components/DwSteps/DwStep.vue
+++ packages/components/DwSteps/DwStep.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center">
     <div v-if="active > index" class="flex-center w-28px h-28px border-rounded-50% bg-#eaf3ff">
-      <!-- <el-icon color="var(--el-color-primary)"><Select /></el-icon> -->
+      <el-icon color="var(--el-color-primary)"><Select /></el-icon>
     </div>
     <span v-else :class="['w-28px h-28px border-rounded-50% flex-center bg-#F2F3F5 color-#4E5969', {'bg-primary color-#fff': active === index}]">{{ index + 1 }}</span>
     <span :class="['pl-10px color-#6E7279', {'color-#292A2C': active === index}]">{{ title }}</span>
