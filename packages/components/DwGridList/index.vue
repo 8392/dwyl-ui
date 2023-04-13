61ed139b68a1b70eaf897a3d84ea0ceb66577762
蔡博 <306048913@qq.com>
2023-04-13 13:17:28 +0800

    修改useList默认参数bug

--- packages/components/DwGridList/index.vue
+++ packages/components/DwGridList/index.vue
@@ -47,7 +47,7 @@ const emits = defineEmits(listEmit)
 const { tableConfig, pageField, limitField, tableData, total, tableLoading, pageData, getList, clickPage, refresh } = useList(props, emits)
 
 const onSizeChange = (e) => {
-  pageData[tableConfig.value.limitField] = e * 5
+  pageData.value[tableConfig.value.limitField] = e * 5
   getList()
 }
 
