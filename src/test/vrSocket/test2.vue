5d13d2f0f6fd2a5541a10c2ab9e29bb3784a5862
蔡博 <306048913@qq.com>
2023-09-12 17:27:44 +0800

    init

--- src/test/vrSocket/test2.vue
+++ src/test/vrSocket/test2.vue
@@ -8,8 +8,10 @@
       <div id="localVideo"></div>
     </div>
 
-    <div>
+    <div class="flex gap-20px mt-20px">
+      <div v-for="(item, index) in userList" :id="`newUser_${item.streamID}`" :key="index" class="newUser">
 
+      </div>
     </div>
   </div>
 </template>
@@ -34,20 +36,21 @@ const loginRoomMeth = async () => {
   }
 }
 
+const config = {
+  camera: {
+    audioInput: 'default',
+    video: true,
+    audio: true,
+    videoQuality: 4,
+    frameRate: 15,
+    width: 1280,
+    height: 720,
+    bitRate: 300
+  }
+}
+
 const startPlayingStreamMeth = async () => {
   const streamID = parseInt(Math.random() * 999999) + ''
-  const config = {
-    camera: {
-      audioInput: 'default',
-      video: true,
-      audio: true,
-      videoQuality: 4,
-      frameRate: 15,
-      width: 1280,
-      height: 720,
-      bitRate: 300
-    }
-  }
 
   const localStream = await zg.createStream(config)
   zg.startPublishingStream(streamID, localStream, { videoCodec: 'VP8' })
@@ -61,6 +64,43 @@ const startPlayingStreamMeth = async () => {
   })
 }
 
+const userList = ref([])
+
+zg.on('roomStreamUpdate', async (roomID, updateType, streamList, extendedData) => {
+  console.log('加入房间----------------------------------你哈啊啊啊十大打撒打撒萨达萨达阿斯顿撒旦撒', streamList)
+  // streams added
+  if (updateType === 'ADD') {
+    userList.value = streamList
+
+    streamList.forEach(async item => {
+      const remoteStream = await zg.startPlayingStream(item.streamID, config)
+
+      const remoteView = zg.createRemoteStreamView(remoteStream)
+      remoteView.play(`newUser_${item.streamID}`, {
+        objectFit: 'cover',
+        enableAutoplayDialog: true
+      })
+
+      console.log('加入房间', item)
+    })
+    // for (let i = 0; i < streamList.length; i++) {
+    //   console.log('加入房间')
+    //   // playMultipleStreamList.push(streamList[i])
+
+    //   // // add item to streamList
+    //   // appendHtml(streamList[i].streamID, streamList[i].user)
+    //   // if (playObj[streamList[i].user.userID]) {
+    //   //   $(`#m-${streamList[i].user.userID}`).attr('data-id', streamList[i].streamID)
+    //   //   stopToStart(playObj[streamList[i].user.userID], streamList[i].streamID)
+    //   //   $(`#s-${streamList[i].user.userID}`).text(streamList[i].streamID)
+    //   // }
+    //   // playObj[streamList[i].user.userID] = streamList[i].streamID
+    // }
+  } else if (updateType === 'DELETE') {
+    console.log('退出房间')
+  }
+})
+
 </script>
 
 <style lang="scss" scoped>
@@ -68,5 +108,10 @@ const startPlayingStreamMeth = async () => {
   width: 400px;
   height: 300px;
   border: 1px solid #dfdfdf;
+}
+.newUser{
+  width: 400px;
+  height: 300px;
+  border: 1px solid #f00;
 }
 </style>
