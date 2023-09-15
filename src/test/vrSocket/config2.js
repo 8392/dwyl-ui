1b2b3fc487b85b3d0673d5e7a9810b627429623d
artist <306048913@qq.com>
2023-09-15 15:45:50 +0800

    aa

--- src/test/vrSocket/config2.js
+++ src/test/vrSocket/config2.js
@@ -3,6 +3,6 @@ export default {
   server: 'wss://webliveroom1781365118-api.imzego.com/ws',
   serverSecret: '70780f88069d31770cfaf2a8b688bcd7',
   roomID: '0001',
-  userID: 'caibo2',
+  userID: 'test001',
   token: '04AAAAAGUFBGEAEG11emJvNmVwbGdvazNiMjcAsGwH/LV7QXb1Dtqt05mNOim8+y1HDCcClZmOs8KqsdjH3/O3fATe2z9gc1ZO/XC3L+HhkXbvlCTicEdqCtA51tj99P0hmLOC+5AZNvIVOOVD56bfJxFNVo6+Axe9Zj3fbyR2qv/awrsj4WP/JyZPsJnCKpM4E3DMR0AOZcy5pZXk564MeiHUeB9T8qK43ep/aUNJRLWXHjpNXBB1oRHSEQUKu6yV9mt31sYj1Y8REEOL'
 }
