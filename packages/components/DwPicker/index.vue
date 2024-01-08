9054e2b14eea8b9a9da8d5eb5f6403b7569bb03a
caibo <306048913@qq.com>
2024-01-08 10:34:43 +0800

    DwPicker组件新增 type="month"类型

--- packages/components/DwPicker/index.vue
+++ packages/components/DwPicker/index.vue
@@ -1,5 +1,6 @@
 <template>
   <el-date-picker
+    v-if="type === 'daterange'"
     v-model="timeData"
     class="vertical-top mr-12px"
     :type="type"
@@ -12,10 +13,20 @@
     @change="changeDate"
   >
   </el-date-picker>
+  <el-date-picker
+    v-if="type === 'month'"
+    v-model="timeData"
+    class="vertical-top mr-12px"
+    :type="type"
+    v-bind="$attrs"
+    :placeholder="placeholder"
+    @change="changeDate"
+  >
+  </el-date-picker>
 </template>
 
 <script lang="jsx" setup>
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 import dayjs from 'dayjs'
 defineOptions({
   name: 'DwPicker'
@@ -39,6 +50,10 @@ const props = defineProps({
     type: String,
     default: '结束日期'
   },
+  placeholder: {
+    type: String,
+    default: '请选择日期'
+  },
   rangeSeparator: {
     type: String,
     default: '-'
@@ -59,10 +74,19 @@ const timeData = ref(null)
 
 watchEffect(() => {
   const { start, end } = props
-  if (start && end) {
-    timeData.value = [start, end]
+  if (props.type === 'daterange') {
+    if (start && end) {
+      timeData.value = [start, end]
+    } else {
+      timeData.value = null
+    }
   } else {
-    timeData.value = null
+    if (start && end) {
+      const resTime = start.split(' ') || []
+      timeData.value = resTime[0]
+    } else {
+      timeData.value = null
+    }
   }
 })
 
@@ -71,11 +95,24 @@ const emit = defineEmits(['update:start', 'update:end', 'change'])
 const changeDate = (val) => {
   let startTime = null
   let endTime = null
-  if (val) {
-    const [start, end] = val
-    startTime = dayjs(start).format('YYYY-MM-DD HH:mm:ss') // 2011-10-17 00:17:56
-    endTime = dayjs(end).format('YYYY-MM-DD HH:mm:ss') // 2011-10-17 00:17:56
+  if (props.type === 'daterange') {
+    if (val) {
+      const [start, end] = val
+      startTime = dayjs(start).format('YYYY-MM-DD HH:mm:ss') // 2011-10-17 00:17:56
+      endTime = dayjs(end).format('YYYY-MM-DD HH:mm:ss') // 2011-10-17 00:17:56
+    }
+  } else {
+    // 选择月份
+    const date = new Date(val)
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const year = date.getFullYear()
+    const day = new Date(year, month, 0)
+    if (val) {
+      startTime = year + '-' + month + '-01 00:00:00'
+      endTime = year + '-' + month + '-' + day.getDate() + ' 23:59:59'
+    }
   }
+
   emit('update:start', startTime)
   emit('update:end', endTime)
   emit('change', {
